e4864d157dbb1c5b8e208374a01315498b7c4798
Jerry Huang <jerry@conic-soft.com>
2024-04-23 14:03:50 +0800

    First Class

---
+++ Ap01/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ap01
+{
+    internal class Person
+    {
+        private string _id, _name;
+        public void Display()
+        {
+            Console.WriteLine($"Id={_id}, Name={_name}");
+        }
+        //signagure(函式簽章)
+        public Person(string id, string name)
+        {
+            this._id = id;
+            this._name = name;
+        }   
+    }
+}
--- Ap01/Program.cs
+++ Ap01/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Ap01;
+
+Person p1 = new Person("C001", "John");
+//p1._id = "XXX";
+p1.Display();
+
